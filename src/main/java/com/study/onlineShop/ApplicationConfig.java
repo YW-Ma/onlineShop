4e628e12d5e5ea512005ecf1d25bab74af521bce
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2021-01-10 03:16:37 +0800

    change back to my project

--- src/main/java/com/study/onlineShop/ApplicationConfig.java
+++ src/main/java/com/study/onlineShop/ApplicationConfig.java
@@ -2,10 +2,6 @@ package com.study.onlineShop;
 
 import java.util.Properties;
 import javax.sql.DataSource;
-
-import com.study.onlineShop.log.Logger;
-import com.study.onlineShop.log.PaymentAction;
-import com.study.onlineShop.log.ServerLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -15,42 +11,32 @@ import com.study.onlineShop.Confidentials.*;
 @Configuration
 public class ApplicationConfig {
 
-  @Bean
-  public ServerLogger logger() {
-    return new ServerLogger();
+  @Bean(name = "sessionFactory")
+  public LocalSessionFactoryBean sessionFactory() {
+    LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+    sessionFactory.setDataSource(dataSource());
+    sessionFactory.setPackagesToScan("com.study.onlineShop.entity");
+    sessionFactory.setHibernateProperties(hibernateProperties());
+    return sessionFactory;
   }
 
-  @Bean
-  public PaymentAction pay() {
-    return new PaymentAction();
+  @Bean(name = "dataSource")
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+    dataSource.setUrl("jdbc:mysql://" + Confidentials.RDS_ENDPOINT + ":3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
+    dataSource.setUsername(Confidentials.USERNAME);
+    dataSource.setPassword(Confidentials.PASSWORD);
+
+    return dataSource;
   }
 
-//  @Bean(name = "sessionFactory")
-//  public LocalSessionFactoryBean sessionFactory() {
-//    LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-//    sessionFactory.setDataSource(dataSource());
-//    sessionFactory.setPackagesToScan("com.study.onlineShop.entity");
-//    sessionFactory.setHibernateProperties(hibernateProperties());
-//    return sessionFactory;
-//  }
-//
-//  @Bean(name = "dataSource")
-//  public DataSource dataSource() {
-//    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//    dataSource.setUrl("jdbc:mysql://" + Confidentials.RDS_ENDPOINT + ":3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
-//    dataSource.setUsername(Confidentials.USERNAME);
-//    dataSource.setPassword(Confidentials.PASSWORD);
-//
-//    return dataSource;
-//  }
-//
-//
-//  private final Properties hibernateProperties() {
-//    Properties hibernateProperties = new Properties();
-//    hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
-//    hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
-//    hibernateProperties.setProperty("hibernate.show_sql", "true");
-//    return hibernateProperties;
-//  }
+
+  private final Properties hibernateProperties() {
+    Properties hibernateProperties = new Properties();
+    hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+    hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+    hibernateProperties.setProperty("hibernate.show_sql", "true");
+    return hibernateProperties;
+  }
 }
--- src/main/java/com/study/onlineShop/log/Logger.java
+++
@@ -1,5 +0,0 @@
-package com.study.onlineShop.log;
-
-public interface Logger {
-  void log(String info);
-}
