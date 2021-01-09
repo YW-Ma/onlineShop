bab6a24f852086ef94a2f3c4fa4911f626f0e697
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2021-01-09 17:46:17 +0800

    use the newer engine

--- src/main/java/com/study/onlineShop/ApplicationConfig.java
+++ src/main/java/com/study/onlineShop/ApplicationConfig.java
@@ -36,8 +36,8 @@ public class ApplicationConfig {
 
   private final Properties hibernateProperties() {
     Properties hibernateProperties = new Properties();
-    hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-    hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect"); // if mysql version is 8, MySQL5InnoDBDialect
+    hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+    hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
     hibernateProperties.setProperty("hibernate.show_sql", "true");
     return hibernateProperties;
   }
